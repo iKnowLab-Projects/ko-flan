b4eda76fe912fd059984d7d05718bd6d1838d7a3
Tom <lop@soongsil.ac.kr>
2023-07-14 15:48:31 +0900

    klue_ynat imple

--- task/__init__.py
+++ task/__init__.py
@@ -2,12 +2,13 @@ import re
 from .nsmc.generate import NSMCGenerator
 from .apeach.generate import APEACHGenerator
 from .korquadv1.generate import KorQuADv1Generator
-
+from .klue_ynat.generate import KlueYnatGenerator
 
 ALL_TASKS = {
     "nsmc": NSMCGenerator, 
     "apeach": APEACHGenerator,
-    "korquad_v1.0": KorQuADv1Generator
+    "korquad_v1.0": KorQuADv1Generator,
+    "klue_ynat": KlueYnatGenerator
     }
 
 

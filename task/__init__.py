a140df36dc37e28e2d49054a852325a6b5c02311
Tom <lop@soongsil.ac.kr>
2023-09-02 20:03:25 +0900

    ah_dialogSummary 추가

--- task/__init__.py
+++ task/__init__.py
@@ -31,6 +31,7 @@ from .mm_2022dialogTopic.generate import mm2022DialogTopicGenerator
 from .mm_2022chatTopic.generate import mm2022ChatTopicGenerator
 from .mm_2022newsTopic.generate import mm2022NewsTopicGenerator
 from .aihub_dialogSummary_topic.generate import aihubDialogSummaryTopicGenerator
+from .aihub_dialogSummary_summary.generate import aihubDialogSummarySummaryGenerator
 from .kor_nli.generate import KorNLIGenerator
 
 ALL_TASKS = {
@@ -65,6 +66,7 @@ ALL_TASKS = {
     "mm_2022ner": mm2022NERGenerator,
     "mm_dialog": mmDialogGenerator,
     "aihub_dialogSummaryTopic": aihubDialogSummaryTopicGenerator,
+    "aihub_dialogSummarySummary": aihubDialogSummarySummaryGenerator,
     "nikl_cb2020": nikl.CB2020Generator,
     "nikl_cb2021": nikl.CB2021Generator,
     "tl_text_entailment": TlTextEntailmentGenerator,

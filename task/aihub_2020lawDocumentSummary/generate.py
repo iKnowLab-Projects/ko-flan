f88105e954a6f330d17be9c36714c656b6c8eb86
Tom <lop@soongsil.ac.kr>
2023-09-04 12:42:30 +0900

    오타수정

--- task/aihub_2020lawDocumentSummary/generate.py
+++ task/aihub_2020lawDocumentSummary/generate.py
@@ -10,18 +10,18 @@ class aihub2020lawDocumentSummaryGenerator(BaseGenerator):
         self.instructions = [
             "글을 읽고 적절한 요약문을 생성하세요.",
             "다음 텍스트를 요약해주세요.",
-            "다음 문서를를 간단한 문장으로 줄여보세요",
-            "다음 문서를를 이해하고 요약해보세요.",
+            "다음 문서를 간단한 문장으로 줄여보세요",
+            "다음 문서를 이해하고 요약해보세요.",
             "주어진 문서를 읽고 적절한 요약문을 만들어 주세요.",
             "주어진 내용을 분석하여 주장을 간추려주세요.",
-            "다음 문장의 내용을 논리적이고 짧게 정리해주세요.",
+            "다음 문장의 내용을 논리적으로 짧게 정리해주세요.",
             "다음을 읽고 어떤 이야기를 하는지 짧게 나타내보세요.",
             "주어진 지문을 해석하고 요약해보세요.",
             "다음 문장들을 읽고 주요 내용을 정리하여 간단히 표현해주세요.",
         ]
     def generate(self, split: str):
         dataset = load_dataset(
-            "iknow-lab/aihub_2020lawDocumentSummary", split=split, token=True
+            "iknow-lab/aihub_2020newsArticleSummary", split=split, token=True
         ).shuffle(seed=42)
         
         for item in dataset:
